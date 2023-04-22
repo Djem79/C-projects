3ce1cda4f08c70b9aa1f5f664da653e1ee260464
Djem79 <tdm.979@gmail.com>
2023-04-22 13:39:07 +0300

    create new example 010 ArrayMethod

---
+++ example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array ={12, 25, 34, 4, 578, 6, 76, 8};
+
+int n = array.Length;
+int find = 76;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
+//console.WriteLine();
